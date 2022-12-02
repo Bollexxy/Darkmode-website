2c5547829f05e0f40c2646d6542c232bb67accea
boluwatifeyemitan7@gmail.com <boluwatifeyemitan7@gmail.com>
2022-12-02 10:37:12 +0100

    Darkmode Website

---
+++ main.js
@@ -0,0 +1,15 @@
+const body = document.body;
+const cont = document.getElementById('cont');
+function darkmode(){
+  body.classList.toggle('dark-theme');
+}
+function lightmode(){
+  body.classList.toggle('dark-theme');
+}
+
+      function showpopup(){
+        cont.classList.toggle('show-popup');
+      }
+      function closepopup() {
+        cont.classList.toggle('show-popup');
+      }
